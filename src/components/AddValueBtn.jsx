ad8502745bc3cc3b035df2addfda9fe0999457f9
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-05-23 19:47:49 +0200

    Finished with on click functionality of component

---
+++ src/components/AddValueBtn.jsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import PropTypes from 'prop-types';
+
+const MIN = 1;
+const MAX = 60;
+
+function AddValueBtn({ id, btnIcon, valueAddedOnClick, stateSetter }) {
+	const onClickHandler = () => stateSetter(state => {
+		const newState = state + valueAddedOnClick;
+
+		return (newState > MAX)? MAX : (newState < MIN)? MIN : newState;
+	});
+
+	return (
+		<button id={id} className='cursor-pointer' onClick={onClickHandler}>
+			<FontAwesomeIcon icon={btnIcon} />
+		</button>
+	);
+}
+
+AddValueBtn.propTypes = {
+	id: PropTypes.string.isRequired,
+	btnIcon: PropTypes.object.isRequired,
+	valueAddedOnClick: PropTypes.number.isRequired,
+	stateSetter: (props, propName, componentName) => {
+		if(typeof(props[propName]) !== 'function') {
+			throw new Error("Invalid type of '" + propName + "' supplied to" + " '" + componentName + "'. Validation failed.");
+		} 
+	}
+}
+
+export default AddValueBtn;
