c1e3382ffa1c4423fada44c6a88cf6516aab86ab
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-05-23 21:09:54 +0200

    Created component to display simple title text

---
+++ src/components/Title.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+function Title({ text }) {
+	return (
+		<header>
+			<h1 className='text-5xl text-primary-green'>{text}</h1>
+		</header>
+	);
+}
+
+Title.propTypes = {
+	text: PropTypes.string.isRequired
+}
+
+export default Title;
