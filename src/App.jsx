e779687e2a9ffe49db6687d6f378ae68a8e7957d
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-05-23 19:46:46 +0200

    Imported component and implemented it
    The DurationControlContainer component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
+import DurationControlContainer from "./components/DurationControlContainer";
+
+const DEFAULT_SESSION_LENGTH = 25;
+const DEFAULT_BREAK_LENGTH = 5;
+
 function App() {
+    const [sessionLength, sessionLengthSetter] = useState(DEFAULT_SESSION_LENGTH);
+    const [breakLength, breakLengthSetter] = useState(DEFAULT_BREAK_LENGTH);
 
 	return (
-		<div className="">
-			<p className="underline">Hello World!</p>
+		<div className="flex gap-40">
+			<DurationControlContainer id="session" state={sessionLength} stateSetter={sessionLengthSetter} />
+			<DurationControlContainer id="break" state={breakLength} stateSetter={breakLengthSetter} />
 		</div>
 	);
 }
