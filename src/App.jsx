5683eff5405ebd46a7abe2c3a162e06f5083ec19
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-05-23 21:10:15 +0200

    Imported and implemented Title component
    As well as wrapped the content of the component
    in a fragment, I didn't use the shorthand <> </>
    because my editor thinks it's an error when it
    isn't.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { useState } from "react";
 
+import Title from "./components/Title";
 import DurationControlContainer from "./components/DurationControlContainer";
 
 const DEFAULT_SESSION_LENGTH = 25;
@@ -10,10 +12,13 @@ function App() {
     const [breakLength, breakLengthSetter] = useState(DEFAULT_BREAK_LENGTH);
 
 	return (
-		<div className="flex gap-40">
-			<DurationControlContainer id="session" state={sessionLength} stateSetter={sessionLengthSetter} />
-			<DurationControlContainer id="break" state={breakLength} stateSetter={breakLengthSetter} />
-		</div>
+		<React.Fragment>
+			<Title text="Let's Start Cooking!" />
+			<div className="flex gap-40">
+				<DurationControlContainer id="session" state={sessionLength} stateSetter={sessionLengthSetter} />
+				<DurationControlContainer id="break" state={breakLength} stateSetter={breakLengthSetter} />
+			</div>
+		</React.Fragment>
 	);
 }
 
