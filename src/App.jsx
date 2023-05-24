643978637bc64118a2d0c1989329274c9cd13de0
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-05-24 19:04:21 +0200

    Imported and implemented Timer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 import Title from "./components/Title";
+import Timer from "./components/Timer";
 import DurationControlContainer from "./components/DurationControlContainer";
 
 import { DEFAULT_VALUES } from "./exports/constants";
@@ -13,6 +14,7 @@ function App() {
 	return (
 		<React.Fragment>
 			<Title text="Let's Start Cooking!" />
+			<Timer />
 			<div className="flex flex-col gap-10 sm:flex-row sm:gap-40">
 				<DurationControlContainer id="session" state={sessionLength} stateSetter={sessionLengthSetter} />
 				<DurationControlContainer id="break" state={breakLength} stateSetter={breakLengthSetter} />
