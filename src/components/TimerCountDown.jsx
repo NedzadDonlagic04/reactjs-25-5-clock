acd13cf6fd5771e48e6c38e5ca895a4217e6b27d
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-05-25 18:40:06 +0200

    Added counting functionality

--- src/components/TimerCountDown.jsx
+++ src/components/TimerCountDown.jsx
@@ -1,39 +1,63 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useRef } from "react";
 
 import PropTypes from 'prop-types';
-import { TIMER_STATE } from "../exports/constants";
+import { DEFAULT_VALUES, TIMER_STATE } from "./../exports/constants";
+import { convertSecondsToMMSS } from "./../exports/functions";
 
 let intervalID = null;
 
-function TimerCountDown({ timerState }) {
-	const [currentlyCounting/* , currentlyCountingSetter */] = useState('Session');
-	//                             ^ commented to prevent warning
+function TimerCountDown({ sessionLength, breakLength, timerState }) {
+	const beepAudio = useRef(null); 
 
-	useEffect(() => {
-		if(timerState === TIMER_STATE.PLAYING) intervalID = setInterval(() => console.log(1), 1000);
-		else {
-			clearInterval(intervalID);
-			if(timerState === TIMER_STATE.RESET) {
-				console.log('Reset');
+	const [display, displaySetter] = useReducer((prev, state) => {
+		if(state === TIMER_STATE.RESET) {
+			prev.text = 'Session';
+			prev.time = sessionLength * 60;
+
+			beepAudio.current.pause();
+			beepAudio.current.currentTime = 0;
+		} else {
+			prev.time--;
+
+			if(prev.time === 0) beepAudio.current.play();
+			else if(prev.time < 0) {
+				prev.time = (prev.text === 'Session')? breakLength * 60 : sessionLength * 60;
+				prev.text = (prev.text === 'Session')? 'Break' : 'Session';
 			}
 		}
-	}, [timerState]);
+
+		prev.formattedTime = convertSecondsToMMSS(prev.time);
+
+		return { ...prev };
+	}, { text: 'Session', time: sessionLength * 60, formattedTime: convertSecondsToMMSS(sessionLength * 60) });
+
+	useEffect(() => {
+		if(timerState === TIMER_STATE.PLAYING) intervalID = setInterval(() => displaySetter(TIMER_STATE.PLAYING), DEFAULT_VALUES.TIMER_DURATION);
+		else {
+			clearInterval(intervalID);
+			if(timerState === TIMER_STATE.WAITING) displaySetter(TIMER_STATE.RESET);
+		}
+	}, [sessionLength, breakLength, timerState]);
 
 	return (
 		<div className="flex flex-col justify-center items-center gap-3 px-14 py-5 border-4 rounded-3xl border-primary-green">
 			<h2 className="text-primary-green text-4xl" id="timer-label">
-				{currentlyCounting}
+				{display.text}
 			</h2>
 
 			<p className="text-primary-green text-3xl">
-				<time id="time-left">25:00</time>
+				<time id="time-left">{display.formattedTime}</time>
 			</p>
+
+			<audio id="beep" preload="auto" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" ref={beepAudio}></audio>
 		</div>	
 	);
 }
 
 TimerCountDown.propTypes = {
+	sessionLength: PropTypes.number.isRequired,
+	breakLength: PropTypes.number.isRequired,
 	timerState: PropTypes.number.isRequired
-}
+};
 
 export default TimerCountDown;
