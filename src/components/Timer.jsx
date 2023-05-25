dddcba4459ac83ca9ea7a7433f63fab46412b25b
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-05-25 14:18:33 +0200

    Changed property type and variable names

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,13 +3,12 @@ import TimerControls from "./TimerControls";
 
 import PropTypes from 'prop-types';
 
-function Timer(/* { sessionLength, breakLength, timerPlaying, timerPlayingSetter } */) {
-	//								^ commented to prevent warning
+function Timer({ /* sessionLength, breakLength, */ timerState, timerStateSetter }) {
 	return (
 		<div className="flex flex-col justify-center items-center gap-5">
-			<TimerCountDown />
+			<TimerCountDown timerState={timerState} />
 			
-			<TimerControls />
+			<TimerControls timerState={timerState} timerStateSetter={timerStateSetter} />
 		</div>
 	);
 }
@@ -17,8 +16,8 @@ function Timer(/* { sessionLength, breakLength, timerPlaying, timerPlayingSetter
 Timer.propTypes = {
 	sessionLength: PropTypes.number.isRequired,
 	breakLength: PropTypes.number.isRequired,
-	timerPlaying: PropTypes.bool.isRequired,
-	timerPlayingSetter: (props, propName, componentName) => {
+	timerState: PropTypes.number.isRequired,
+	timerStateSetter: (props, propName, componentName) => {
 		if(typeof(props[propName]) !== 'function') {
 			throw new Error("Invalid type of '" + propName + "' supplied to" + " '" + componentName + "'. Validation failed.");
 		} 
