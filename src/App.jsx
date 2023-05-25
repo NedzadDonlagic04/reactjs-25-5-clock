9bcf84f8d9c38c4d3c22d9e6e24268869bf1e2e8
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-05-25 14:17:01 +0200

    Changed variable names due to added TIMER_STATE

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import Title from "./components/Title";
 import Timer from "./components/Timer";
 import DurationControlContainer from "./components/DurationControlContainer";
 
-import { DEFAULT_VALUES } from "./exports/constants";
+import { DEFAULT_VALUES, TIMER_STATE } from "./exports/constants";
 
 function App() {
     const [sessionLength, sessionLengthSetter] = useState(DEFAULT_VALUES.SESSION);
     const [breakLength, breakLengthSetter] = useState(DEFAULT_VALUES.BREAK);
-	const [timerPlaying, timerPlayingSetter] = useState(DEFAULT_VALUES.TIMER_PLAYING);
+	const [timerState, timerStateSetter] = useState(TIMER_STATE.RESET);
 
 	return (
 		<React.Fragment>
 			<Title text="Let's Start Cooking!" />
-			<Timer sessionLength={sessionLength} breakLength={breakLength} timerPlaying={timerPlaying} timerPlayingSetter={timerPlayingSetter} />
+			<Timer sessionLength={sessionLength} breakLength={breakLength} timerState={timerState} timerStateSetter={timerStateSetter} />
 			<div className="flex flex-col gap-10 sm:flex-row sm:gap-40">
-				<DurationControlContainer id="session" state={sessionLength} stateSetter={sessionLengthSetter} disabled={timerPlaying} />
-				<DurationControlContainer id="break" state={breakLength} stateSetter={breakLengthSetter} disabled={timerPlaying} />
+				<DurationControlContainer id="session" state={sessionLength} stateSetter={sessionLengthSetter} disabled={timerState} />
+				<DurationControlContainer id="break" state={breakLength} stateSetter={breakLengthSetter} disabled={timerState} />
 			</div>
 		</React.Fragment>
 	);
